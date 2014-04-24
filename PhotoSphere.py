05d3cd01be1c5315f9db2bfcbf367e6da8aef522
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-04-24 22:52:49 +0200

    rm trailing spaces

--- PhotoSphere.py
+++ PhotoSphere.py
@@ -5,10 +5,10 @@
 #
 # Original Notes:
 # -----
-# This code is not an ideal example of Pythonic coding or use of OO 
-# techniques. It is a simple and direct exposition of how to use the 
-# Open GL API in Python via the PyOpenGL package. It also uses GLUT, 
-# a high quality platform independent library. Due to using these APIs, 
+# This code is not an ideal example of Pythonic coding or use of OO
+# techniques. It is a simple and direct exposition of how to use the
+# Open GL API in Python via the PyOpenGL package. It also uses GLUT,
+# a high quality platform independent library. Due to using these APIs,
 # this code is more like a C program using procedural programming.
 #
 # To run this example you will need:
@@ -27,7 +27,7 @@ from PhotoSphereRenderer import *        # Draw (), Initialize () and all the re
 from ArcBall import *        # // *NEW* ArcBall header
 from PyQt4 import QtGui, QtCore
 
-# *********************** Globals *********************** 
+# *********************** Globals ***********************
 # Python 2.2 defines these directly
 try:
     True
@@ -49,7 +49,7 @@ fileUrl = ""
 
 # Reshape The Window When It's Moved Or Resized
 def ReSizeGLScene(Width, Height):
-    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small 
+    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small
         Height = 1
 
     glViewport(0, 0, Width, Height)        # Reset The Current Viewport And Perspective Transformation
@@ -66,7 +66,7 @@ def ReSizeGLScene(Width, Height):
     return
 
 
-# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
+# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
 def keyPressed(*args):
     global g_quadratic
     # If escape is pressed, kill everything.
@@ -82,19 +82,19 @@ def main():
     # pass arguments to init
     glutInit(sys.argv)
 
-    # Select type of Display mode:   
-    #  Double buffer 
+    # Select type of Display mode:
+    #  Double buffer
     #  RGBA color
-    # Alpha components supported 
+    # Alpha components supported
     # Depth buffer
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    
-    # get a 640 x 480 window 
+
+    # get a 640 x 480 window
     glutInitWindowSize(640, 480)
-    
-    # the window starts at the upper left corner of the screen 
+
+    # the window starts at the upper left corner of the screen
     glutInitWindowPosition(0, 0)
-    
+
     # Okay, like the C version we retain the window id to use when closing, but for those of you new
     # to Python, remember this assignment would make the variable local and not global
     # if it weren't for the global declaration at the start of main.
@@ -102,19 +102,19 @@ def main():
 
        # Register the drawing function with glut, BUT in Python land, at least using PyOpenGL, we need to
     # set the function pointer and invoke a function to actually register the callback, otherwise it
-    # would be very much like the C version of the code.    
+    # would be very much like the C version of the code.
     glutDisplayFunc(Draw)
-    
+
     # Uncomment this line to get full screen.
     #glutFullScreen()
 
     # When we are doing nothing, redraw the scene.
     glutIdleFunc(Draw)
-    
+
     # Register the function called when our window is resized.
     glutReshapeFunc(ReSizeGLScene)
-    
-    # Register the function called when the keyboard is pressed.  
+
+    # Register the function called when the keyboard is pressed.
     glutKeyboardFunc(keyPressed)
 
 
@@ -127,13 +127,13 @@ def main():
 
     # We've told Glut the type of window we want, and we've told glut about
     # various functions that we want invoked (idle, resizing, keyboard events).
-    # Glut has done the hard work of building up thw windows DC context and 
+    # Glut has done the hard work of building up thw windows DC context and
     # tying in a rendering context, so we are ready to start making immediate mode
     # GL calls.
     # Call to perform inital GL setup (the clear colors, enabling modes
     Initialize (640, 480, fileUrl)
 
-    # Start Event Processing Engine    
+    # Start Event Processing Engine
     glutMainLoop()
 
 # Print message to console, and kick off the main to get it rolling.
@@ -146,43 +146,43 @@ def main():
 
 
 class Example(QtGui.QWidget):
-    
+
     def __init__(self):
         super(Example, self).__init__()
-        
+
         self.initUI()
-        
-    def initUI(self):      
+
+    def initUI(self):
 
         self.btn = QtGui.QPushButton('OK', self)
         self.btn.move(20, 20)
         self.btn.clicked.connect(self.showDialog)
-        
+
         self.le = QtGui.QLineEdit(self)
         self.le.move(130, 22)
-        
+
         self.fdBtn = QtGui.QPushButton('Choose', self)
         self.fdBtn.move(280, 20)
         self.fdBtn.clicked.connect(self.chooseFile)
-        
+
         self.setGeometry(300, 300, 390, 150)
         self.setWindowTitle('Input dialog')
         self.show()
-        
+
     def chooseFile(self):
         sFileName =  QtGui.QFileDialog.getOpenFileName(self, "Open File", "","Files (*.*)" )
         self.le.setText("file:///"+sFileName)
         self.showDialog()
-        
+
     def showDialog(self):
         global fileUrl
         text = (str(self.le.text()))
         fileUrl = text
         QtCore.QCoreApplication.instance().quit()
-            
-        
+
+
 def qtmain():
-    
+
     app = QtGui.QApplication(sys.argv)
     ex = Example()
     ret = app.exec_()
--- PhotoSphereRenderer.py
+++ PhotoSphereRenderer.py
@@ -13,7 +13,7 @@ from ArcBall import *                 # ArcBallT and this tutorials set of point
 
 PI2 = 2.0*3.1415926535            # 2 * PI (not squared!)         // PI Squared
 
-# *********************** Globals *********************** 
+# *********************** Globals ***********************
 # Python 2.2 defines these directly
 try:
     True
@@ -71,7 +71,7 @@ g_pctOffset=0.15
 def loadImage( imageName = "/local/photo/SR71.jpg" ): # "/local/photo/montagna.jpg" ):
     global g_texWidth
     global g_texHeight
-    global g_texData    
+    global g_texData
 
 
 
@@ -82,45 +82,45 @@ def loadImage( imageName = "/local/photo/SR71.jpg" ): # "/local/photo/montagna.j
 #        g_texWidth, g_texHeight, g_texData = im.size[0], im.size[1], im.tostring("raw", "RGBA", 0, -1)
 #    except SystemError:
 #       g_texWidth, g_texHeight, g_texData = im.size[0], im.size[1], im.tostring("raw", "RGBX", 0, -1)
-#       
-#       
+#
+#
 #    im = Image.open("/home/paolo/Desktop/GLBALL/GLball/wall01.tga")
     width, height = im.size
-    
+
     pixels = im.getdata()
     pixelData = im.load()
-    
+
     c  = numpy.zeros(width * int (height * (1 + 2*g_pctOffset))  * 3, numpy.uint8)
     rowSkip = int (height*g_pctOffset)
-    
+
     curHeight =     int (height * (1+2*g_pctOffset))
-    
-    for hj in range(curHeight):    
+
+    for hj in range(curHeight):
         for i in range(width):
             j = hj - rowSkip
             if (j >= 0) and (j < height):
                 d = pixelData[i,j]
                 c[3*(hj*width + i) ] = d[0]
                 c[3*(hj*width + i)+1 ] = d[1]
-                c[3*(hj*width + i)+2 ] = d[2] 
-            
+                c[3*(hj*width + i)+2 ] = d[2]
+
     g_texWidth, g_texHeight, g_texData = width, height, c
     g_texWidth, g_texHeight, g_texData = width, curHeight, c
-    
+
 #    imOutput = Image.new( 'RGB', (width,height), "black") # create a new black image
 #    pixelsOutput = imOutput.load() # create the pixel map
-#    for j in range(height):    
-#        for i in range(width): 
+#    for j in range(height):
+#        for i in range(width):
 #            pixelsOutput[i,j] = (c[3*(j*width + i) ], c[3*(j*width + i)+1 ], c[3*(j*width + i)+2 ])
-#        
-#    
+#
+#
 #    imOutput.show()
 
-# A general OpenGL initialization function.  Sets all of the initial parameters. 
+# A general OpenGL initialization function.  Sets all of the initial parameters.
 def Initialize (Width, Height, fileUrl):                # We call this right after our OpenGL window is created.
     global g_quadratic
     global g_textures
-    
+
     #loadImage("file:///local/www/web/wci/uppete/ups/PANO_20130727_160023403796945.jpg")
     #loadImage("http://bonas.us/uppete/ups/PANO_20130727_160023403796945.jpg")
     loadImage(fileUrl)
@@ -139,9 +139,9 @@ def Initialize (Width, Height, fileUrl):                # We call this right aft
     gluQuadricOrientation(g_quadratic,GLU_INSIDE)
     gluQuadricOrientation(g_quadratic,GLU_OUTSIDE)
     gluQuadricNormals(g_quadratic, GLU_SMOOTH);
-    gluQuadricDrawStyle(g_quadratic, GLU_FILL); 
-    gluQuadricTexture(g_quadratic, GL_TRUE); 
-    # Why? this tutorial never maps any textures?! ? 
+    gluQuadricDrawStyle(g_quadratic, GLU_FILL);
+    gluQuadricTexture(g_quadratic, GL_TRUE);
+    # Why? this tutorial never maps any textures?! ?
     # gluQuadricTexture(g_quadratic, GL_TRUE);            # // Create Texture Coords
 
     glEnable (GL_LIGHT0)
@@ -153,7 +153,7 @@ def Initialize (Width, Height, fileUrl):                # We call this right aft
     glEnable ( GL_TEXTURE_2D );
     glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
     glGenTextures (1, g_textures);
-    
+
     glBindTexture ( GL_TEXTURE_2D, g_textures[0] );
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
@@ -194,12 +194,12 @@ def Upon_Drag (cursor_x, cursor_y):
         fZoom = g_fLastScale + fZoomAmt
         fZoom = numpy.clip(10.0,0.05, fZoom)
         g_fLastScale = fZoom
-        
+
         g_Scaling = getScalingF(fZoom)
-        
-                
+
+
         pass
-    
+
     g_Transform = copy.copy (g_Rotation)
     g_Transform = Matrix3fMulMatrix3f(g_Transform, g_Translation)
     g_Transform = Matrix3fMulMatrix3f(g_Transform, g_Scaling)
@@ -222,14 +222,14 @@ def Upon_Click (button, button_state, cursor_x, cursor_y):
     elif (button == GLUT_RIGHT_BUTTON and button_state == GLUT_DOWN):
         g_rightDragging = True
         mouse_pt = Point2fT (cursor_x, cursor_y)
-        g_ArcBall.click (mouse_pt);    
+        g_ArcBall.click (mouse_pt);
         pass
     elif (button == GLUT_MIDDLE_BUTTON and button_state == GLUT_UP):
         g_midDragging = False
         pass
     elif (button == GLUT_MIDDLE_BUTTON and button_state == GLUT_DOWN):
         mouse_pt = Point2fT (cursor_x, cursor_y)
-        g_ArcBall.click (mouse_pt);            
+        g_ArcBall.click (mouse_pt);
         g_midDragging = True
         pass
     elif (button == GLUT_LEFT_BUTTON and button_state == GLUT_UP):
@@ -245,7 +245,7 @@ def Upon_Click (button, button_state, cursor_x, cursor_y):
 
 
 
-def Torus(MinorRadius, MajorRadius):        
+def Torus(MinorRadius, MajorRadius):
     # // Draw A Torus With Normals
     glBegin( GL_TRIANGLE_STRIP );                                    # // Start A Triangle Strip
     for i in xrange (20):                                             # // Stacks
@@ -274,16 +274,16 @@ def Torus(MinorRadius, MajorRadius):
 
 def Draw ():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);                # // Clear Screen And Depth Buffer
-    glLoadIdentity();  
+    glLoadIdentity();
     if 0:                                              # // Reset The Current Modelview Matrix
         glTranslatef(-1.5,0.0,-6.0);                                    # // Move Left 1.5 Units And Into The Screen 6.0
-    
+
         glPushMatrix();                                                    # // NEW: Prepare Dynamic Transform
         glMultMatrixf(g_Transform);                                        # // NEW: Apply Dynamic Transform
         glColor3f(0.75,0.75,1.0);
         Torus(0.30,1.00);
         glPopMatrix();                                                    # // NEW: Unapply Dynamic Transform
-    
+
         glLoadIdentity();                                                # // Reset The Current Modelview Matrix
         glTranslatef(0.0,0.0,-0.30);                                        # // Move Right 1.5 Units And Into The Screen 7.0
 
@@ -300,17 +300,17 @@ def Draw ():
         gluSphere(g_quadratic,g_sphereRadius,50,50);
     else:
         glBegin(GL_QUADS);
-        glColor4f(1.0,1.0,1.0,1.0); 
-        glNormal3f(0,0,1); 
-        glTexCoord2f(0,0); 	
+        glColor4f(1.0,1.0,1.0,1.0);
+        glNormal3f(0,0,1);
+        glTexCoord2f(0,0);
         glVertex3f(-1,-1,0);
-        glTexCoord2f(1,0);	
+        glTexCoord2f(1,0);
         glVertex3f(1,-1,0);
-        glTexCoord2f(1,1);	
+        glTexCoord2f(1,1);
         glVertex3f(1,1,0);
-        glTexCoord2f(0,1);	
+        glTexCoord2f(0,1);
         glVertex3f(-1,1,0);
-        glEnd();    
+        glEnd();
     glBindTexture(GL_TEXTURE_2D, 0)
     glPopMatrix();                                                    # // NEW: Unapply Dynamic Transform
 
